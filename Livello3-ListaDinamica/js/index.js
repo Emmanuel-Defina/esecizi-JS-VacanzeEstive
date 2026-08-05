6f2e25b27c588b6900c3598c3b36d157deab2845
Emmanuel-Defina <emmanueldefina2017@gmail.com>
2026-08-05 20:30:39 +0200

    lvl3-ListaDinamica

---
+++ Livello3-ListaDinamica/js/index.js
@@ -0,0 +1,32 @@
+"use strict"
+
+let Lista = [];
+let j = 0;
+
+const txtElem = document.getElementById("txtElem");
+const h3Lista = document.getElementById("h3Lista");
+
+function AggiungiElemento()
+{
+    let Elemento = document.getElementById("txtElemento").value;
+
+    h3Lista.innerHTML = "";
+
+    Lista[j] = Elemento;
+
+    j++;
+         
+     for(let i = 0; i < Lista.length; i++)
+    {
+        h3Lista.innerHTML += "- " + Lista[i] + "<br>";
+    }
+
+    Elemento.value = "";
+         
+}
+
+Elemento.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    AggiungiElemento();
+  }
+});
