ee4482988d52a98648c71d0b8ad8391545a58dec
Emmanuel-Defina <emmanueldefina2017@gmail.com>
2026-08-13 22:30:20 +0200

    lvl5-ResetGioco

---
+++ Livello5-ResetGioco/js/index.js
@@ -0,0 +1,79 @@
+"use strict"
+
+let div = document.getElementById("divGriglia")
+let celle = [];
+let divCella = [];
+let rnd = 0;
+
+let punteggio = 0;
+
+let h2Vittoria = document.getElementById("h2Vittoria");
+
+window.onload = function ()
+{
+    Inizio();
+    
+    for(let i = 0; i<20; i++)
+    {
+        let d = document.createElement("div");
+        d.classList.add("div");
+        d.setAttribute("id", `div-${i}`);
+        d.innerHTML = i;
+        d.addEventListener("click", function(){
+             console.log("Ho cliccato", d.id);
+            Controlla(d.id);
+        })
+        celle[i] = d;
+        div.append(d);
+
+        divCella[i] = {nome : "mare", posizione : i, occupata : false};
+
+        switch(i)
+        {
+            case 16:
+                divCella[i] = {nome : "mare", posizione : i, occupata : true};
+                break;
+            case 17:
+                divCella[i] = {nome : "mare", posizione : i, occupata : true};
+                break;
+            case 18:
+                divCella[i] = {nome : "mare", posizione : i, occupata : true};
+                break;
+        }
+    }
+}
+
+function Inizio()
+{
+    rnd = 0;
+    celle = [];
+    punteggio = 0;
+
+    h2Vittoria.classList.add("nascondi");
+
+    console.log("ONLOAD PARTITO");
+}
+
+function Controlla(id)
+{
+    let i = id.split('-')[1];
+
+    console.log("i =", i);
+    console.log("cella =", divCella[i]);
+
+        if(divCella[i].occupata == true)
+        {
+           punteggio += 1;
+           console.log("PUNTEGGIO =", punteggio);
+        }
+
+    if(punteggio == 3)
+    {
+      h2Vittoria.classList.remove("nascondi");
+    }
+}
+
+function reset()
+{
+    Inizio();
+}
